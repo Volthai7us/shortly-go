de87f7c30370fe3c3ff5b81f7886e1cc46e22f30
Emir Soyturk <emirsytrk@gmail.com>
2023-07-27 15:26:34 +0300

    Add main function to demonstrate URL shortening

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "shortly/app/helper"
+import (
+	"fmt"
+	"shortly/app/entities"
+	"shortly/app/repositories"
+)
 
 func main() {
-helper.Help()
+	urlRepository := repositories.NewURLRepository()
+
+	url := entities.NewURL("https://www.google.com", "abc")
+	urlRepository.Store(url)
+
+	url, found := urlRepository.Find("abc")
+	if found {
+		fmt.Printf("URL: %s\n", url.GetOriginalURL())
+	} else {
+		fmt.Println("URL not found")
+	}
 }
