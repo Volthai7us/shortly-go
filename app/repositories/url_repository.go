d97352a528199546e754eb19577b59f2843bb691
Emir Soyturk <emirsytrk@gmail.com>
2023-07-27 15:21:30 +0300

    Implement URLRepository for in-memory storage

---
+++ app/repositories/url_repository.go
@@ -0,0 +1,26 @@
+package repositories
+
+import "shortly/app/entities"
+
+// in-memory repository
+type URLRepository struct {
+    urls map[string]*entities.URL
+}
+
+// new
+func NewURLRepository() *URLRepository {
+    return &URLRepository{
+        urls: make(map[string]*entities.URL),
+    }
+}
+
+// store
+func (r *URLRepository) Store(url *entities.URL) {
+    r.urls[url.GetShortURL()] = url
+}
+
+// find
+func (r *URLRepository) Find(shortURL string) (*entities.URL, bool) {
+    url, found := r.urls[shortURL]
+    return url, found
+}
